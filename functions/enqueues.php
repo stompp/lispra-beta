e8abf24abacf9baba63504fa5357c6750bbc87f6
J.M. Tomé <josemiguel.tomejimenez@gmail.com>
2016-09-17 17:27:01 +0200

    Added styles and scrypts enqueues Plugins enqueues lispra css and js

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,10 +8,16 @@ function lispra_enqueues() {
 
     /* Styles */
 
+    wp_register_style('lispra-css', get_lispra_lib_path().'css/lispra.css', false, null);
+    wp_enqueue_style('lispra-css');
     /* Scripts */
+    
+    wp_register_script('lispra-js', get_lispra_lib_path().'js/lispra.js', false, null, true);
+    wp_enqueue_script('lispra-js');
 
 }
 
-add_action('wp_enqueue_scripts', 'lispra_enqueues', 100);
+add_action('wp_enqueue_scripts', 'lispra_enqueues', 101);
 
 
+?>
--- lispra-beta.php
+++ lispra-beta.php
@@ -2,14 +2,25 @@
 /*
 Plugin Name: LispraBeta
 Plugin URI:  https://bitbucket.org/jmtome/lispra-beta
-Description: LIspras bestas plujinss
+Description: LispraBeta Beta Test Plugin
 Version: 1.0.0
-Author: Chemi
-Author URI: wplispra.riggitt.org
+Author: J.M Tomé
+Author URI: 
 License: GPLv2
 */
 
+/** Absolute path to the Lispra Lib */
+define('LISPRA_LIB_PATH', '/lispra/');
+
+function get_lispra_lib_path(){
+    return LISPRA_LIB_PATH;
+}
+
+/** enqueues Styles and Scripts */
+require_once dirname(__FILE__) . '/functions/enqueues.php';
+/** [short-codes] */
 require_once dirname(__FILE__) . '/functions/short_codes.php';
+/** Controllers */
 require_once dirname(__FILE__) . '/controllers/setup.php';
 
 
