b03527fc8d9dbbd329040b2d135eee2140998270
J.M. Tomé <josemiguel.tomejimenez@gmail.com>
2016-09-23 00:51:31 +0200

    Forgotten commit Enqueues needed for lispra now included from plugin

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,48 +8,16 @@
 
 function lispra_enqueues() {
 
-//    /** Styles **/
-//
-//    /* Font Awesome */
-//    wp_register_style('fontawesome', get_lispra_lib_path().'vendors/font-awesome/css/font-awesome.min.css', false, null);
-//    wp_enqueue_style('fontawesome');
-//
-//    /* PNotify */
-//    wp_register_style('pnotify', get_lispra_lib_path().'vendors/pnotify/dist/pnotify.css', false, null);
-//    wp_enqueue_style('pnotify');
-//    wp_register_style('pnotify-buttons', get_lispra_lib_path().'vendors/pnotify/dist/pnotify.buttons.css', false, null);
-//    wp_enqueue_style('pnotify-buttons');
-//    wp_register_style('pnotify-nonblock', get_lispra_lib_path().'vendors/pnotify/dist/pnotify.nonblock.css', false, null);
-//    wp_enqueue_style('pnotify-nonblock');
-//    
-//     /* Lispra */
-//    wp_register_style('lispra-css', get_lispra_lib_path().'css/lispra.css', false, null);
-//    wp_enqueue_style('lispra-css');
-//    
-//    /** Scripts **/
-//   
-//    /* PNotify */
-//    wp_register_script('pnotify-js', get_lispra_lib_path().'vendors/pnotify/dist/pnotify.js', false, null, true);
-//    wp_enqueue_script('pnotify-js');
-//    wp_register_script('pnotify-buttons-js', get_lispra_lib_path().'vendors/pnotify/dist/pnotify.buttons.js', false, null, true);
-//    wp_enqueue_script('pnotify-buttons-js');
-//    wp_register_script('pnotify-nonblock-js', get_lispra_lib_path().'vendors/pnotify/dist/pnotify.nonblock.js', false, null, true);
-//    wp_enqueue_script('pnotify-nonblock-js');
-//    
-//    /* Lispra */
-//    wp_register_script('lispra-js', get_lispra_lib_path().'js/lispra.js', false, null, true);
-//    wp_enqueue_script('lispra-js');
-//    get_lispra_lib_directory_uri
-
-    /*     * STYLES* */
 
 
+    /**STYLES**/
+
+    /* Bootstrap */
     wp_register_style('bootstrap', lispra_lib_uri('vendors/bootstrap/dist/css/bootstrap.min.css'), false, null);
     wp_enqueue_style('bootstrap');
     /* Font Awesome */
     wp_register_style('fontawesome', lispra_lib_uri('vendors/font-awesome/css/font-awesome.min.css'), false, null);
     wp_enqueue_style('fontawesome');
-
     /* PNotify */
     wp_register_style('pnotify', lispra_lib_uri('vendors/pnotify/dist/pnotify.css'), false, null);
     wp_enqueue_style('pnotify');
@@ -57,27 +25,22 @@ function lispra_enqueues() {
     wp_enqueue_style('pnotify-buttons');
     wp_register_style('pnotify-nonblock', lispra_lib_uri('vendors/pnotify/dist/pnotify.nonblock.css'), false, null);
     wp_enqueue_style('pnotify-nonblock');
-
-     /* Gentelella */
-//    wp_register_style('gentelella', lispra_lib_uri('css/gentelella.css'), false, null);
-//    wp_enqueue_style('gentelella');
-    
+    /*Gentelella */
+    wp_register_style('gentelella', lispra_lib_uri('css/gentelella.css'), false, null);
+    wp_enqueue_style('gentelella');
     /* Lispra */
     wp_register_style('lispra-css', lispra_lib_uri('css/lispra.css'), false, null);
     wp_enqueue_style('lispra-css');
+    /**!STYLES**/
 
-    /*     * !STYLES* */
-
-    /*     * SCRIPTS* */
+    /**SCRIPTS**/
 
     /* JQuery */
     wp_register_script('jquery-js', lispra_lib_uri('vendors/jquery/dist/jquery.min.js'), false, null, true);
     wp_enqueue_script('jquery-js');
-
     /* Bootstrp */
     wp_register_script('bootstrap-js', lispra_lib_uri('vendors/bootstrap/dist/js/bootstrap.min.js'), false, null, true);
     wp_enqueue_script('bootstrap-js');
-
     /* PNotify */
     wp_register_script('pnotify', lispra_lib_uri('vendors/pnotify/dist/pnotify.js'), false, null, true);
     wp_enqueue_script('pnotify');
@@ -85,17 +48,13 @@ function lispra_enqueues() {
     wp_enqueue_script('pnotify-buttons');
     wp_register_script('pnotify-nonblock', lispra_lib_uri('vendors/pnotify/dist/pnotify.nonblock.js'), false, null, true);
     wp_enqueue_script('pnotify-nonblock');
-
-      /* Gentelella */   
-//    wp_register_script('gentelella-js', lispra_lib_uri('js/gentelella.js'), false, null, true);
-//    wp_enqueue_script('gentelella-js');
-
-    
+     /* Gentelella */   
+    wp_register_script('gentelella-js', lispra_lib_uri('js/gentelella.js'), false, null, true);
+    wp_enqueue_script('gentelella-js');
     /* Lispra */
     wp_register_script('lispra-js', lispra_lib_uri('js/lispra.js'), false, null, true);
     wp_enqueue_script('lispra-js');
-
-    /*     * !SCRIPTS* */
+    /**!SCRIPTS**/
 }
 
 add_action('wp_enqueue_scripts', 'lispra_enqueues', 100);
